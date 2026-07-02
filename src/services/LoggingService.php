ce27eadf1595fc3f85f80e4dedf7486802f772ed
bhlindemann <bh@lindemannrock.com>
2026-07-03 01:21:37 +0400

    fix: correct copyright year in LoggingService.php

--- src/services/LoggingService.php
+++ src/services/LoggingService.php
@@ -5,7 +5,7 @@
  * Service providing logging utilities and helper methods
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2025-2026 LindemannRock
  */
 
 namespace lindemannrock\logginglibrary\services;
