320779f04e55b6b17d70070eee2b0d28b57ce57f
bhlindemann <bh@lindemannrock.com>
2026-07-03 12:50:08 +0400

    fix: update copyright year in LoggingTrait.php

--- src/traits/LoggingTrait.php
+++ src/traits/LoggingTrait.php
@@ -5,7 +5,7 @@
  * Reusable logging trait for consistent logging across plugins
  *
  * @link      https://lindemannrock.com
- * @copyright Copyright (c) 2025 LindemannRock
+ * @copyright Copyright (c) 2025-2026 LindemannRock
  */
 
 namespace lindemannrock\logginglibrary\traits;
